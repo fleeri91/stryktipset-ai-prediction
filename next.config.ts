06a056fe2773bd5f79d7d0c49f3183004d52f742
Flemming Eriksson <fleeri91@gmail.com>
2025-12-01 15:35:13 +0100

    tensorflow config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.externals = [...config.externals, '@tensorflow/tfjs-node']
+    }
+    return config
+  },
 }
 
 export default nextConfig
